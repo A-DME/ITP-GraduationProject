4bd209f9b5367e8a549f8ed439f7084ee0cf5b76
MennaYaserSetait <menna.setait@gmail.com>
2024-02-25 01:15:53 +0200

    design-checkforConflict

---
+++ E-CommerceApp/Modules/ProductInfo/View/ProductPositionsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductPositionsCollectionViewCell.swift
+//  E-CommerceApp
+//
+//  Created by Menna Setait on 24/02/2024.
+//
+
+import UIKit
+
+class ProductPositionsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productPositionImage: UIImageView!
+}
