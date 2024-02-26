eadafb1a306376649806b78c0d5bb659acf7345f
A-DME <201727138@std.sci.cu.edu.eg>
2024-02-26 14:45:09 +0200

    dummy deletion & price and edit on cart cell
    cart cell now has a shadow behind it and increase and decrease button are functioning properly

--- E-CommerceApp/Modules/Cart/ViewModel/CartViewModel.swift
+++ E-CommerceApp/Modules/Cart/ViewModel/CartViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class CartViewModel{
+    var bindResultToViewController: (()->()) = {}
+    var totalPrice: Double? {
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    
+    var cart = [CartProduct(name: "productName 1", price: 46, quantity: 1, img: "ad"),
+                CartProduct(name: "productName 2", price: 88, quantity: 2, img: "ad")]
+    init(){
+        calculateTotalPrice()
+    }
+    
+    func calculateTotalPrice(){
+        totalPrice = 0
+        for product in cart{
+            totalPrice! += product.price * Double(product.quantity)
+        }
+    }
+}
+
+struct CartProduct{
+    var name: String
+    var price: Double
+    var quantity: Int
+    var img: String
+}
