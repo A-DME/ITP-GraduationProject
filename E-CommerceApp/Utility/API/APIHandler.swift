b1a654bd912dbdefb0cc9cc4c263c8843ce8ab16
A-DME <201727138@std.sci.cu.edu.eg>
2024-02-28 23:16:01 +0200

    added new func.  (for GET requests) and added some endpoints
    the function makes url strings , and added endpoints for single order, addresses & address of a customer

--- E-CommerceApp/Utility/API/APIHandler.swift
+++ E-CommerceApp/Utility/API/APIHandler.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class APIHandler{
+    static let ssl = "https://"
     static let storeURL = "q2-23-24-ios-sv-team2.myshopify.com"
     static let accessToken = "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"
     static let apiKey = "ea12d3c9a7ec864490ccdbb44084163a"
@@ -17,9 +18,13 @@ class APIHandler{
         case priceRule
         case customers
         case Customer(id: String)
+        case allAddressesOf(customer_id: String)
+        case address(customer_id: String, address_id: String)
         case orders
+        case order(id: String)
         case products
-        case draftOrder
+        case draftOrders
+        case draftOrder(id: String)
         case discountCode
         case CollectionID(id:String)
         case ProductDetails(id:String)
@@ -32,12 +37,20 @@ class APIHandler{
                 return"customers.json "
             case .Customer(id: let id):
                 return "customers/\(id).json"
+            case .allAddressesOf(customer_id: let customer_id):
+                return "customers/\(customer_id)/addresses.json?limit=1"
+            case .address(customer_id: let customer_id, address_id: let address_id):
+                return "customers/\(customer_id)/addresses/\(address_id).json"
             case .orders:
                 return "orders.json"
+            case .order(id: let id):
+                return "orders/\(id).json"
             case .products:
                 return "products.json"
-            case .draftOrder:
+            case .draftOrders:
                 return "draft_orders.json"
+            case .draftOrder(id: let id):
+                return "draft_orders/\(id).json"
             case .discountCode:
                 return "discount_codes.json"
             case .CollectionID(id: let collectionId):
@@ -49,6 +62,15 @@ class APIHandler{
                 
             }
         }
+    }
+    
+    enum Completions: String{
+        case ssl = "https://"
+        case api_version = "2024-01"
+    }
+    
+    class func urlForGetting(_ endpoint: EndPoints) -> String {
+        return "\(Completions.ssl.rawValue)\(apiKey):\(accessToken)@\(storeURL)/admin/api/\(Completions.api_version.rawValue)/\(endpoint.order)"
     }
     
 }
