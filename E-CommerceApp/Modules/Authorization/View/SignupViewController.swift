69d8eb42ab9c0e53f47399a5bfc98e82cd2f77ce
MennaYaserSetait <menna.setait@gmail.com>
2024-02-29 09:33:17 +0200

    Authorization
    signUp view model: get all customers, post customer/userdefaults/network manager:registerCustomer

--- E-CommerceApp/Modules/Authorization/View/SignupViewController.swift
+++ E-CommerceApp/Modules/Authorization/View/SignupViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
+
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var lblValidation: UILabel!
     @IBOutlet weak var firstName: UITextField!
     
     @IBOutlet weak var secondName: UITextField!
@@ -19,23 +21,79 @@ class SignupViewController: UIViewController {
     
     @IBOutlet weak var passwordConfirmation: UITextField!
     
+    var _firstName, _lastName, _email, _password: String!
+    let userDefualt = Utilities()
+    var signUpViewModel = SignUpViewModel()
+    var isFromLogin: Bool = false
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblValidation.isHidden = true
+        bindToViewModel()
 
         // Do any additional setup after loading the view.
     }
     
-
-    func registerCustomer(firstName: String, lastName: String, email: String, password: String, completion: @escaping (Bool)->Void ) {
-        
-    }
+    
 
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true)
     }
     
     @IBAction func signUpButton(_ sender: Any) {
+        _firstName = firstName.text
+        _lastName = secondName.text
+        _email = emailAddress.text
+        _password = password.text
+        if _firstName != "" && _lastName != ""{
+            if userDefualt.isValidEmail(_email){
+                if _password.count >= 6{
+                    signUpViewModel.registerCustomer(firstName: _firstName , lastName: _lastName , email: _email, password: _password){ result in
+                        print("signUpViewModel.registerCustomer")
+                        switch result{
+                        case true:
+                            print("from view  \(String(describing: self._firstName ?? ""))")
+                        case false:
+                            DispatchQueue.main.async {
+                                self.lblValidation.isHidden = false
+                                self.lblValidation.text = "This user already exists"
+                            }
+                        }
+                    }
+                }else{
+                    lblValidation.isHidden = false
+                    lblValidation.text = "Password must be more than 5 digit "
+                }
+                
+            }else{
+                lblValidation.isHidden = false
+                lblValidation.text = "Please, enter valid email"
+            }
+        }else{
+            lblValidation.isHidden = false
+            lblValidation.text = "Required full name "
+        }
     }
+    
+    func bindToViewModel(){
+        signUpViewModel.bindNavigate = { [weak self] in
+            DispatchQueue.main.async {
+                
+                self?.navigate()
+            }
+        }
+        
+    }
+   
+    func navigate(){
+            if self.signUpViewModel.navigate == true{
+                if self.isFromLogin == true{
+                performSegue(withIdentifier: "toHome", sender: self)
+                }else{
+                    print("failed")
+                }
+            }
+    }
+    
 }
---
+++ E-CommerceApp/Modules/Authorization/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,159 @@
+//
+//  SignUpViewModel.swift
+//  E-CommerceApp
+//
+//  Created by Menna Setait on 28/02/2024.
+//
+
+import Foundation
+class SignUpViewModel{
+    let userDefualt = Utilities()
+    
+    let model = ReachabilityManager()
+    
+    var networkHandler:NetworkManager?
+    
+    var bindResultToViewController : (()->()) = {}
+    
+    var customers : [CustomerModel] = []
+    
+    var flag: Bool = false
+
+    var bindNavigate:(()->()) = {}
+    
+    var bindDontNavigate:(()->()) = {}
+    
+    var navigate: Bool! {
+        didSet{
+            bindNavigate()
+        }
+    }
+    var result : CustomerModel?{
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    
+    init() {
+        self.networkHandler = NetworkManager()
+    }
+    var listOfCustomer : [CustomerModel]?{
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    func getCustomersArray()-> [CustomerModel]?{
+        return listOfCustomer
+    }
+    func getAllCustomers() {
+        let endpoint = APIHandler.EndPoints.customers.order
+        let shopURL = APIHandler.storeURL
+        let apiKey = APIHandler.apiKey
+        let accessToken = APIHandler.accessToken
+        let apiUrl = "https://\(apiKey):\(accessToken)@\(shopURL)/admin/api/2024-01/\(endpoint)"
+        print(apiUrl)
+        networkHandler?.fetch(url: apiUrl, type: AllCustomers.self) { data in
+            print("the data from fetching all customers\(data?.customers ?? [])")
+            if let data = data{
+                print(data.customers ?? [])
+                print("data=customers")
+                self.listOfCustomer = data.customers ?? []
+                print("in view")
+            }else {
+                print("error in getting all customers")
+                
+            }
+        }
+    }
+    func checkNetworkReachability(completion: @escaping (Bool) -> Void) {
+        model.checkNetworkReachability { isReachable in
+            completion(isReachable)
+        }
+    }
+    
+    func registerCustomer(firstName: String, lastName: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        var flag = false
+        print(flag)
+        bindResultToViewController = { [weak self] in
+            print("load.5")
+            self?.customers = self?.getCustomersArray() ?? []
+        }
+        for item in customers {
+            let comingMail = item.email ?? ""
+            if comingMail == email {
+                flag = true
+            }
+        }
+        
+        if flag == false {
+            let customer = CustomerModel(
+                first_name: firstName,
+                last_name: lastName,
+                email: email,
+                phone: nil,
+                tags: password,
+                id: nil,
+                verified_email: true,
+                //addresses: nil,
+                note: "0"
+            )
+            
+            let newCustomer = Customer(customer: customer)
+            self.registerNewCustomer(customer: newCustomer) { result in
+                switch result {
+                case true:
+                    completion(true)
+                    self.navigate = true
+                case false:
+                    completion(false)
+                    self.navigate = false
+                }
+            }
+        } else {
+            self.navigate = false
+            completion(false)
+        }
+    }
+    
+
+
+
+    func registerNewCustomer(customer: Customer, completion: @escaping (Bool)->Void ){
+        networkHandler?.registerCustomer(newCustomer: customer) {[weak self] (data, response, error )in
+            if error != nil{
+                print(error!)
+                completion(false)
+            }else{
+                if let data = data{
+                    let json = try! JSONSerialization.jsonObject(with: data, options:
+                            .allowFragments) as! Dictionary<String, Any>
+                    let savedCustomer = json["customer"] as? Dictionary<String,Any>
+                    let id = savedCustomer?["id"] as? Int ?? 0
+                    let customerName = savedCustomer?["first_name"] as? String ?? ""
+                    let customerLastName = savedCustomer?["last_name"] as? String ?? ""
+                    let customerEmail = savedCustomer?["email"] as? String ?? ""
+                    let customerPassword = savedCustomer?["tags"] as? String ?? ""
+                    let customerNote = savedCustomer?["note"] as? String ?? "0"
+                    if id != 0 {
+                        self?.userDefualt.login()
+                        self?.userDefualt.addCustomerId(id: id)
+                        self?.userDefualt.addCustomerEmail(customerEmail: customerEmail)
+                        self?.userDefualt.addCustomerName(customerName: "\(customerName) \(customerLastName)")
+                        self?.userDefualt.login()
+                        self?.userDefualt.setUserPassword(password: customerPassword)
+                        self?.userDefualt.setUserNote(note: customerNote)
+                        print("passwordUserrrr\( String(describing: self?.userDefualt.getUserPassword()))")
+                        completion(true)
+                        print("add to userDefualt successfully!!!")
+                    }else{
+                        completion(false)
+                        //self?.navigate = false
+                        print("error to register")
+                    }
+                }
+            }
+        }
+    }
+    
+    
+}
