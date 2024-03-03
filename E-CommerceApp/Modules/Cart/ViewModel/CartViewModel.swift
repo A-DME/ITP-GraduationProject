6fec7a820157945a5451fa4ddb0a216c5422f5e9
A-DME <201727138@std.sci.cu.edu.eg>
2024-03-04 00:36:50 +0200

    edits in cart now reflected to api

--- E-CommerceApp/Modules/Cart/ViewModel/CartViewModel.swift
+++ E-CommerceApp/Modules/Cart/ViewModel/CartViewModel.swift
@@ -15,28 +15,38 @@ class CartViewModel{
         }
     }
     
-//    var cart = [CartProduct(name: "productName 1", price: 46, quantity: 4, availableQuantity: 5, img: "ad"),
-//                CartProduct(name: "productName 2", price: 88, quantity: 2, availableQuantity: 3, img: "ad")]
     init(){
         networkManager = NetworkManager()
     }
-    
+// MARK: - Awaiting customer's cart id
     func loadData(){
-        networkManager?.fetch(url: APIHandler.urlForGetting(.draftOrder(id: "1148388933877")), type: DraftOrderContainer.self, complitionHandler: { container in
+        networkManager?.fetch(url: APIHandler.urlForGetting(.draftOrder(id: "1148431433973")), type: DraftOrderContainer.self, complitionHandler: { container in
             self.cart = container?.draftOrder.lineItems
         })
+    }
+    
+    func extractLineItemsPostData(lineItems: [LineItem]) -> [[String: Any]]{
+        var result: [[String: Any]] = []
+        for item in lineItems{
+            var properties : [[String: String]] = []
+            for property in item.properties {
+                properties.append(["name":property.name, "value": property.value])
+            }
+            result.append(["variant_id": item.variantID, "quantity": item.quantity, "properties": properties])
+        }
+                
+        return result
+    }
+    
+    func updateOrder(cartItems: [LineItem]?){
+        guard let cartItems = cartItems else { return }
+        
+//        print(extractLineItemsPostData(lineItems: cartItems))
+        networkManager?.putInApi(url: APIHandler.urlForGetting(.draftOrder(id: "1148431433973")), parameters: ["draft_order": ["line_items": extractLineItemsPostData(lineItems: cartItems)]])
     }
     
     func getCart() -> [LineItem]{
         return cart ?? []
     }
     
-}
-
-struct CartProduct{
-    var name: String
-    var price: Double
-    var quantity: Int
-    var availableQuantity: Int
-    var img: String
 }
