df05269425fa545502102e45d08c4fadcd3401e0
MennaYaserSetait <menna.setait@gmail.com>
2024-03-04 11:42:34 +0200

    signing in
    sign in view model, sign in view controller[ sign in story and handling credentilas cases]

--- E-CommerceApp/Modules/Authorization/ViewModel/SignInViewModel.swift
+++ E-CommerceApp/Modules/Authorization/ViewModel/SignInViewModel.swift
@@ -7,27 +7,44 @@
 
 import Foundation
 class SignInViewModel{
+    var networkHandler:NetworkManager?
     let model = ReachabilityManager()
+    let userDefualt = Utilities()
+    var bindResultToViewController : (()->()) = {}
     
-    
+    init() {
+        self.networkHandler = NetworkManager()
+    }
+    var listOfCustomer : [CustomerModel]?{
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    func getAllCustomers()-> [CustomerModel]?{
+        return listOfCustomer
+    }
     func checkNetworkReachability(completion: @escaping (Bool) -> Void) {
         model.checkNetworkReachability { isReachable in
             completion(isReachable)
         }
     }
-//    func loadData(){
-//        let endpoint = APIHandler.EndPoints.Customer(id: id)
-//        let shopURL = APIHandler.storeURL
-//        let apiKey = APIHandler.apiKey
-//        let accessToken = APIHandler.accessToken
-//
-//        let apiUrl = "https://\(apiKey):\(accessToken)@\(shopURL)/admin/api/2024-01/\(endpoint)"
-//        networkHandler?.fetch(url: apiUrl, type: Products.self, complitionHandler: { data in
-//            self.result = data
-//      
-//        })
-//        
-//        
-//    }
+    func loadData() {
+        let apiUrl = "https://a73c5fc1c095fd186d957dd2093e9006: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@q2-24-team2.myshopify.com/admin/api/2024-01/customers.json?since_id=1"
+        print(apiUrl)
+        networkHandler?.fetchCustomers(url: apiUrl, type: AllCustomers.self) { data in
+            //print("the data from fetching all customers\(data?.customers?.count ?? 0)")
+            if let data = data{
+                self.listOfCustomer = data.customers
+                print("no of customers in load data\(self.listOfCustomer?.count)")
+            }else {
+                print("error in getting all customers")
+                
+            }
+        }
+    }
+    
+    func checkDraftOrderInUser(){}
+    
+   
     
 }
