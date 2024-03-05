2eda2476e11b427ebc3228718c4c2d9b4bb1cc69
MennaYaserSetait <menna.setait@gmail.com>
2024-03-05 18:20:24 +0200

    Add To Cart/Wishlist
    added separate defaults for cart and wishlist ids,edited navbar, addToCart

--- E-CommerceApp/Modules/ProductInfo/ViewModel/ProductInfoViewModel.swift
+++ E-CommerceApp/Modules/ProductInfo/ViewModel/ProductInfoViewModel.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 class ProductInfoViewModel{
-    
+    let dummyLineItem: [String: Any] = ["title": "dummy", "quantity": 1, "price": "0.0", "properties":[]]
     var networkHandler:NetworkManager?
+    
+    var userDefaults : Utilities?
     
     var bindResultToViewController : (()->()) = {}
     
@@ -19,6 +21,7 @@ class ProductInfoViewModel{
     }
     init(){
         networkHandler = NetworkManager()
+        userDefaults = Utilities()
     }
     func getProductDetails()-> Product?{
         return product
@@ -39,6 +42,35 @@ class ProductInfoViewModel{
       
         })
     }
+    func extractLineItemsPostData(lineItems: [LineItem]) -> [[String: Any]]{
+        var result: [[String: Any]] = []
+        for item in lineItems{
+            var properties : [[String: String]] = []
+            for property in item.properties {
+                properties.append(["name":property.name, "value": property.value])
+            }
+            result.append(["variant_id": item.variantID ?? 0, "quantity": item.quantity, "properties": properties])
+        }
+                
+        return result
+    }
+    //["draft_order": ["line_items": cartItems.count != 0 ? extractLineItemsPostData(lineItems: cartItems)
     
     
+    func updateCartItems(cartItems: [LineItem]?){
+        guard let cartItems = cartItems else { return }
+        print(extractLineItemsPostData(lineItems: cartItems))
+        networkHandler?.putInApi(url: APIHandler.urlForGetting(.draftOrder(id: self.userDefaults?.getCartID() ?? "")), parameters: ["draft_order": ["line_items": cartItems.count != 0 ? extractLineItemsPostData(lineItems: cartItems) : [dummyLineItem]]])
+        Thread.sleep(forTimeInterval: 1)
+        
+        
+    }
+    
+//    func updateWishlist(wishItems: [LineItem]?){
+//        guard let wishItems = wishItems else { return }
+//
+//        print(extractLineItemsPostData(lineItems: wishItems))
+//        networkHandler?.putInApi(url: APIHandler.urlForGetting(.draftOrder(id: self.userDefaults?.getWishlistID() ?? "")), parameters: ["draft_order": ["line_items": wishItems.count != 0 ? extractLineItemsPostData(lineItems: wishItems) : [dummyLineItem]]])
+//    }
+
 }
