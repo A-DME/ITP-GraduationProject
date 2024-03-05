c0c35f16b768e96359cf3628f1508eccf3cd6299
A-DME <201727138@std.sci.cu.edu.eg>
2024-03-05 20:04:23 +0200

    added applePay feature (Complete)
    complete dummy

--- E-CommerceApp/Modules/Payment/ViewModel/PaymentViewModel.swift
+++ E-CommerceApp/Modules/Payment/ViewModel/PaymentViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import PassKit
 
 class PaymentViewModel{
     let paymentMethods = ["Online payment", "Cash on delivery"]
@@ -33,6 +34,16 @@ class PaymentViewModel{
         })
     }
     
+    func configurePaymentRequest(request: PKPaymentRequest){
+        request.merchantIdentifier = "merchant.com.my.shopify.pay"
+        request.supportedNetworks = [.masterCard, .visa]
+        
+        request.supportedCountries = ["EG", "US"]
+        request.merchantCapabilities = .threeDSecure
+        request.countryCode = "EG"
+        request.currencyCode = UserDefaults.standard.string(forKey: "currencyTitle") ?? "USD"
+    }
+    
     func completeOrder(draftId: Int){
         
 //        print(extractLineItemsPostData(lineItems: cartItems))
@@ -48,7 +59,7 @@ class PaymentViewModel{
         if flag == false{
             postOrder()
         }else{
-            let order = Order(id: 0, lineItems: priceUpdatedCart(getFilteredCart()), createdAt: draftOrder?.updatedAt ?? "", currency: UserDefaults.standard.string(forKey: "currencyTitle") ?? "", currentSubtotalPrice: "", name: "", subtotalPrice: "", totalPrice: "", customer: draftOrder?.customer ?? CustomerModel(first_name: "", last_name: "", email: "", phone: "", tags: "", id: 0, verified_email: false, note: ""), currentTotalDiscounts: "", totalDiscounts: "",appliedDiscount: draftOrder?.appliedDiscount)
+            let order = Order(id: 0, lineItems: priceUpdatedCart(getFilteredCart()), createdAt: draftOrder?.updatedAt ?? "", currency: UserDefaults.standard.string(forKey: "currencyTitle") ?? "", currentSubtotalPrice: "", name: "", subtotalPrice: "", totalPrice: "", customer: draftOrder?.customer ?? CustomerModel(first_name: "", last_name: "", email: "", phone: "123", tags: "", id: 0, verified_email: false, note: ""), currentTotalDiscounts: "", totalDiscounts: "",appliedDiscount: draftOrder?.appliedDiscount)
             let parameters = HelperFunctions().convertToDictionary(object: order, String: "order") ?? [:]
             networkManager?.PostToApi(url: APIHandler.urlForGetting(.orders), parameters: parameters)
         }
@@ -74,6 +85,16 @@ class PaymentViewModel{
         for item in 0...cart.count-1 {
             result.append(cart[item])
             result[item].price = String(UserDefaults.standard.double(forKey: "factor") * (Double(result[item].price) ?? 0.0))
+        }
+        
+        return result
+    }
+    
+    func getOrderTotalPrice() -> Double{
+        let cart = getFilteredCart()
+        var result: Double = 0
+        for item in cart {
+            result += UserDefaults.standard.double(forKey: "factor") * Double(item.quantity) * (Double(item.price) ?? 0.0)
         }
         
         return result
