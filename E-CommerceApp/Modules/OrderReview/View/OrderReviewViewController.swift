c4bba598ad5a04d3bc97c1b0b542791e54e27a85
A-DME <201727138@std.sci.cu.edu.eg>
2024-03-05 03:35:29 +0200

    applyCoupon in orderReview (not put to api)

--- E-CommerceApp/Modules/OrderReview/View/OrderReviewViewController.swift
+++ E-CommerceApp/Modules/OrderReview/View/OrderReviewViewController.swift
@@ -28,6 +28,8 @@ class OrderReviewViewController: UIViewController,UICollectionViewDelegate,UICol
     
     var viewModel: OrderReviewViewModel?
     
+    var discountValue = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -35,12 +37,12 @@ class OrderReviewViewController: UIViewController,UICollectionViewDelegate,UICol
         itemsCollection.delegate = self
         itemsCollection.dataSource = self
         itemsCollection.register(ItemsCollectionViewCell.nib(), forCellWithReuseIdentifier: "ItemCell")
+        self.viewModel = OrderReviewViewModel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         viewModel?.checkNetworkReachability{ isReachable in
             if isReachable {
-                self.viewModel = OrderReviewViewModel()
                 self.reloadView()
             } else {
                 DispatchQueue.main.async {
@@ -98,18 +100,25 @@ class OrderReviewViewController: UIViewController,UICollectionViewDelegate,UICol
             subtotalPrice += (UserDefaults.standard.double(forKey: "factor") * (Double(product.price) ?? 0.0)) * Double(product.quantity)
         }
         self.subTotal.text = "\(String(format: "%.2f",subtotalPrice)) \(UserDefaults.standard.string(forKey: "currencyTitle") ?? "USD")"
-        let discountValue = 20.0
-        self.discount.text = "-\("\(String(discountValue))") \(UserDefaults.standard.string(forKey: "currencyTitle") ?? "USD")"
-        self.totalPrice.text = "\(String(format: "%.2f",subtotalPrice - discountValue)) \(UserDefaults.standard.string(forKey: "currencyTitle") ?? "USD")"
+        self.discount.text = "\("\(String(discountValue))") \(UserDefaults.standard.string(forKey: "currencyTitle") ?? "USD")"
+        self.totalPrice.text = "\(String(format: "%.2f",subtotalPrice + discountValue)) \(UserDefaults.standard.string(forKey: "currencyTitle") ?? "USD")"
     }
     
     @IBAction func applyDiscount(_ sender: Any) {
 // MARK: - Todo: applying discount logic
         if applyDiscountButton.titleLabel?.text == "Apply"{
+            let amount = viewModel!.check(discountCode: discountField.text ?? "")
+            print(amount)
+            if amount != 0.0 {
+                discountValue = amount
+                print(discountValue)
+                calculateLabels()
+            }
             applyDiscountButton.setTitle("Change", for: .normal)
             discountField.isUserInteractionEnabled = false
             discountField.alpha = 0.5
         } else {
+            viewModel?.clearRuleId()
             applyDiscountButton.setTitle("Apply", for: .normal)
             discountField.isUserInteractionEnabled = true
             discountField.alpha = 1.0
@@ -129,6 +138,7 @@ class OrderReviewViewController: UIViewController,UICollectionViewDelegate,UICol
         // Pass the selected object to the new view controller.
     }
     @IBAction func proceedToAddress(_ sender: Any) {
+        viewModel?.putDiscount(draftId: draftId)
         performSegue(withIdentifier: "proceed", sender: nil)
     }
 }
--- E-CommerceApp/Modules/OrderReview/ViewModel/OrderReviewViewModel.swift
+++ E-CommerceApp/Modules/OrderReview/ViewModel/OrderReviewViewModel.swift
@@ -11,6 +11,8 @@ class OrderReviewViewModel{
     var networkManager: NetworkManager?
     var bindResultToViewController: (()->()) = {}
     let model = ReachabilityManager()
+    var discounts: [PriceRule]?
+    var ruleId = -1
     var cart: [LineItem]? {
         didSet{
             bindResultToViewController()
@@ -31,14 +33,49 @@ class OrderReviewViewModel{
         networkManager?.fetch(url: APIHandler.urlForGetting(.draftOrder(id: "\(String(draftId))")), type: DraftOrderContainer.self, complitionHandler: { container in
             self.cart = container?.draftOrder.lineItems
         })
+        networkManager?.fetch(url: APIHandler.urlForGetting(.priceRule), type: PriceRules.self, complitionHandler: { container in
+            self.discounts = container?.priceRules
+        })
     }
     
+    func clearRuleId(){ ruleId = -1 }
     
-    func apply(discount: PriceRule){
+    func setRuleId(id: Int){ ruleId = id }
+    
+    
+    func check(discountCode: String) -> Double {
+        var discountAmount = 0.0
+        guard let discounts = discounts else { return discountAmount }
+        for rule in 0 ..< discounts.count{
+            if discountCode == discounts[rule].title{
+                setRuleId(id: rule)
+                discountAmount = Double(discounts[rule].value) ?? 0.0
+                print("Discount code exixts, amount: \(discountAmount)")
+                break
+            }
+        }
+        
+        print("if nothing above, not available")
+        return discountAmount
 //        guard let cartItems = cartItems else { return }
 //        
 ////        print(extractLineItemsPostData(lineItems: cartItems))
 //        networkManager?.putInApi(url: APIHandler.urlForGetting(.draftOrder(id: "\(String(dummyDraftId))")), parameters: ["draft_order": ["line_items": extractLineItemsPostData(lineItems: cartItems)]])
+    }
+    
+    func convertRuleToDiscount(rule: PriceRule) -> [String: Any]{
+        var result: [String: Any] = [:]
+        result["title"] = rule.title
+        result["value"] = rule.value
+        result["value_type"] = rule.valueType
+        return result
+    }
+    
+    func putDiscount(draftId: Int){
+        if ruleId > -1 {
+            print(convertRuleToDiscount(rule: discounts![ruleId]))}
+        networkManager?.putInApi(url: APIHandler.urlForGetting(.draftOrder(id: "\(String(draftId))")), parameters: ["draft_order": ["applied_discount": (ruleId > -1 ? convertRuleToDiscount(rule: discounts![ruleId]) : nil) ]])
+        
     }
     
     
