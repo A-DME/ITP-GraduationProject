e48daf17c299bf6a0b8376f0705b1ca458e3de94
MennaYaserSetait <menna.setait@gmail.com>
2024-03-05 07:32:18 +0200

    Post Draft Order:(added to user defaults)

--- E-CommerceApp/Modules/Authorization/ViewModel/SignUpViewModel.swift
+++ E-CommerceApp/Modules/Authorization/ViewModel/SignUpViewModel.swift
@@ -23,6 +23,10 @@ class SignUpViewModel{
     
     var bindDontNavigate:(()->()) = {}
     
+    var wishlistDraftId : Int?
+    
+    var cartDraftId : Int?
+    
     var navigate: Bool! {
         didSet{
             bindNavigate()
@@ -66,9 +70,7 @@ class SignUpViewModel{
             }
         }
     }
-    func uploadData(){
-        
-    }
+    
     func checkNetworkReachability(completion: @escaping (Bool) -> Void) {
         model.checkNetworkReachability { isReachable in
             completion(isReachable)
@@ -76,7 +78,7 @@ class SignUpViewModel{
     }
     
     func registerCustomer(firstName: String, lastName: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
-        
+        self.createDraftOrder()
         print("Flag in view Model is:\(flag)")
         if flag == false {
             
@@ -91,7 +93,7 @@ class SignUpViewModel{
                 //addresses: nil,
                 note: "0"
             )
-            //let newCustomer = Customer(customer: customer)
+            
             self.registerNewCustomer(customer: customer) { result in
                 switch result {
                 case true:
@@ -102,6 +104,7 @@ class SignUpViewModel{
                     self.navigate = false
                     completion(false)
                     
+                    
                 }
             }
         } else {
@@ -111,62 +114,89 @@ class SignUpViewModel{
     }
     
     func registerNewCustomer(customer: CustomerModel, completion: @escaping (Bool)->Void ){
-            guard let newCustomer = HelperFunctions().convertToDictionary(object: customer, String: "customer") else { return }
-            print(newCustomer)
-            let apiUrl = String ("https://a73c5fc1c095fd186d957dd2093e9006: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@q2-24-team2.myshopify.com/admin/api/2024-01/customers.json?since_id=1 ")
+        guard let newCustomer = HelperFunctions().convertToDictionary(object: customer, String: "customer") else { return }
+        print(newCustomer)
+        let apiUrl = APIHandler.urlForGetting(.customers)
+        
+        
+        networkHandler?.postWithResponse(url: apiUrl, type: Customer.self, parameters: newCustomer) {
+            customerContainer in
             
-     
-            networkHandler?.postWithResponse(url: apiUrl, type: Customer.self, parameters: newCustomer) {
-                customerContainer in
+            print("calling PostCustomerToAPi")
+            
+            if let savedCustomer = customerContainer?.customer {
+                let id = savedCustomer.id ?? 0
+                print("1:\(id)\n")
                 
-                print("calling PostCustomerToAPi")
+                let customerName = savedCustomer.first_name ?? ""
+                print("1:\(customerName)\n")
                 
-                if let savedCustomer = customerContainer?.customer {
-                    let id = savedCustomer.id ?? 0
-                    print("1:\(id)\n")
+                let customerLastName = savedCustomer.last_name ?? ""
+                print("1:\(customerLastName)\n")
+                
+                let customerEmail = savedCustomer.email ?? ""
+                print("1:\(customerEmail)\n")
+                
+                let customerPassword = savedCustomer.tags ?? ""
+                print("1:\(customerPassword)\n")
+                
+                let customerNote = savedCustomer.note ?? "0"
+                print("1:\(customerPassword)\n")
+                
+                if id != 0 {
                     
-                    let customerName = savedCustomer.first_name ?? ""
-                    print("1:\(customerName)\n")
+                    self.userDefualt.login()
+                    self.userDefualt.addCustomerId(id: id)
+                    self.userDefualt.addCustomerEmail(customerEmail: customerEmail)
+                    self.userDefualt.addCustomerName(customerName: "\(customerName) \(customerLastName)")
+                    self.userDefualt.login()
+                    self.userDefualt.setUserPassword(password: customerPassword)
                     
-                    let customerLastName = savedCustomer.last_name ?? ""
-                    print("1:\(customerLastName)\n")
+                    self.userDefualt.setUserNote(note: "\(self.cartDraftId ?? 0),\(self.wishlistDraftId ?? 0)")
+                    print(self.userDefualt.getUserNote())
                     
-                    let customerEmail = savedCustomer.email ?? ""
-                    print("1:\(customerEmail)\n")
+                    print("Password User:\(String(describing: self.userDefualt.getUserPassword()))")
                     
-                    let customerPassword = savedCustomer.tags ?? ""
-                    print("1:\(customerPassword)\n")
+                    completion(true)
+                    self.navigate = true
+                    print("add to userDefualt successfully!!!")
                     
-                    let customerNote = savedCustomer.note ?? "0"
-                    print("1:\(customerPassword)\n")
-                    
-                    if id != 0 {
-                        
-                        self.userDefualt.login()
-                        self.userDefualt.addCustomerId(id: id)
-                        self.userDefualt.addCustomerEmail(customerEmail: customerEmail)
-                        self.userDefualt.addCustomerName(customerName: "\(customerName) \(customerLastName)")
-                        self.userDefualt.login()
-                        self.userDefualt.setUserPassword(password: customerPassword)
-                        self.userDefualt.setUserNote(note: customerNote)
-                        
-                        print("Password User:\(String(describing: self.userDefualt.getUserPassword()))")
-                        
-                        completion(true)
-                        self.navigate = true
-                        print("add to userDefualt successfully!!!")
-                        
-                    } else {
-                        completion(false)
-                        self.navigate = false
-                        print("No customer found in the response.")
-                    }
-                        
-                    
+                } else {
+                    completion(false)
+                    self.navigate = false
+                    print("No customer found in the response.")
                 }
+                
+                
             }
         }
+    }
     
+    func createDraftOrder() {
+        let dummyLineItem :[String : Any] =
+        [
+            "title": "dummy",
+            "quantity": 1,
+            "price": "0.0",
+            "properties":[]
+        ]
+        
+        let apiUrl = APIHandler.urlForGetting(.draftOrders)
+        networkHandler?.postWithResponse(url: apiUrl, type: DraftOrderContainer.self, parameters: ["draft_order": ["line_items":[dummyLineItem]]], completion: { container in
+            
+            self.cartDraftId = container?.draftOrder.id
+        })
+        Thread.sleep(forTimeInterval: 1)
+        
+        networkHandler?.postWithResponse(url: apiUrl, type: DraftOrderContainer.self, parameters: ["draft_order": ["line_items":[dummyLineItem]]], completion: { container in
+            
+            self.wishlistDraftId = container?.draftOrder.id
+        })
+        Thread.sleep(forTimeInterval: 1)
 
+    }
+        
+        
+        
     
 }
