666257581cd5cc95b0c7904cba98a0ab3dc53f0b
A-DME <201727138@std.sci.cu.edu.eg>
2024-03-05 12:15:17 +0200

    based on current customer id

--- E-CommerceApp/Modules/Addresses/ViewModel/AddNewAddressViewModel.swift
+++ E-CommerceApp/Modules/Addresses/ViewModel/AddNewAddressViewModel.swift
@@ -8,14 +8,16 @@
 import Foundation
 class AddNewAddressViewModel{
     var networkManager: NetworkManager?
+    var userDefault: Utilities?
     var helper: HelperFunctions?
 
     init() {
         self.networkManager = NetworkManager()
+        userDefault = Utilities()
         self.helper = HelperFunctions()
     }
     
     func postAddressToApi(customer_id: Int,name: String, phone: String, city: String, address: String, setDefault: Bool){
-        networkManager?.PostToApi(url: APIHandler.urlForGetting(.allAddressesOf(customer_id: String(customer_id))), parameters: ["address":["name":name,"phone":phone, "address1": address, "city": city, "default": setDefault]])
+        networkManager?.PostToApi(url: APIHandler.urlForGetting(.allAddressesOf(customer_id: String(userDefault?.getCustomerId() ?? 0))), parameters: ["address":["name":name,"phone":phone, "address1": address, "city": city, "default": setDefault]])
     }
 }
